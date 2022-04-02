52e571b7e9a51788c06cfc9450ac06c76ecd73c4
sabiyevaaa03 <sabiyevaaa03@gmail.com>
2022-04-02 13:09:56 +0300

    LAB9 for web-technologies Sabiyeva Amina

---
+++ src/components/Body.js
@@ -0,0 +1,52 @@
+import './Body.css'
+
+const Body = () =>{
+    return (
+        <div className='Body'>
+            <div className='newsList'>Some Information
+
+                <div className='newsBlockImg1'>
+                    <div className='newsBlockText1'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>
+                        </div>
+
+                <div className='newsBlockImg2'>
+                    <div className='newsBlockText2'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                        </div>
+
+                <div className='newsBlockImg3'>
+                    <div className='newsBlockText3'>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
+                        </div>
+
+                <div className='newsBlockImg4'>
+                    <div className='newsBlockText4'>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with 
+                    desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                        </div>
+
+                <div className='newsBlockImg5'>
+                    <div className='newsBlockText5'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</div>
+                        </div>
+
+                <div className='newsBlockImg5'>
+                    <div className='newsBlockText5'>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here,
+                     content here', making it look like readable English.</div>
+                        </div>
+
+                <div className='newsBlockImg5'>
+                    <div className='newsBlockText5'>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,
+                     and a search for 'lorem ipsum' will uncover many web sites still in their infancy. </div>
+                        </div>
+
+                <div className='newsBlockImg5'>
+                    <div className='newsBlockText5'>Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</div>
+                        </div>
+                            </div>
+
+            <div className='newsTools'>newsTools</div>
+                        
+
+        </div>
+    );
+}
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import './Header.css'
+
+const Header = () =>{
+    return (
+        
+        <div className='Header'>
+            <div className='logo'></div>
+            <div className='headImg'> <h3> Lorem Ipsum </h3> </div>
+
+
+            <div className='ddMenu1'>Number 1</div>
+            <div className='ddMenu2'>Number 2</div>
+            <div className='ddMenu3'>Number 3</div>
+            <div className='ddMenu4'>Number 4</div>
+            <div className='ddMenu5'>Number 5</div>
+            <div className='ddMenu6'>Number 6</div>
+
+            <div className='icon1'>W</div>
+            <div className='icon2'>A</div>
+            <div className='icon3'>Y</div>
+        </div>
+    );
+}
+
+export default Header;
